556f940c73f4572fb512e1dba788173136059d01
Arandfed <federico.aranda@csm.edu.mx>
2024-10-06 13:24:42 -0600

    Draft RPG

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Player player1 = new Player("Default");
+
+        System.out.println("Hello Traveler, what's your name?");
+        // TO DO: implementing functionality to input name
+        System.out.println("Hello" + " " + player1.getname);
+     }
+}
