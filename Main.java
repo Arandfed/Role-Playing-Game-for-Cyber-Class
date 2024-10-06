a2317715fcd3badbbf77a2a4ab4b9b5a1020423b
Arandfed <federico.aranda@csm.edu.mx>
2024-10-06 14:29:32 -0600

    Basic Functionality of an RPG Lobby

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Player player1 = new Player("Default");
 
         System.out.println("Hello Traveler, what's your name?");
-        // TO DO: implementing functionality to input name
-        System.out.println("Hello" + " " + player1.getname);
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        scanner.close();
+
+        player1.setName(name);
+
+        System.out.println("Hello" + " " + player1.getname());
      }
 }
